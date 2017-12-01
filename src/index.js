9b0e3508773eaa6741e9dba943c2353ce0701e54
unknown <you@example.com>
2017-12-01 11:54:21 -0700

    second try

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+// import [variableName] from 'libraryName' //OR './mycode' for own library
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'; //CSS files don't need variableName, can have specific things imported from them
+import App from './App'; //.js extension is default, so not necessary to write // ../parentfolder/gotoOther
+// import registerServiceWorker from './registerServiceWorker'; Windows gives trouble with this code line
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
