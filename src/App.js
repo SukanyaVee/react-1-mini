edb7d6888a6bc17d89ad635c20af75fb23193bcc
unknown <you@example.com>
2017-12-01 12:34:16 -0700

    step 5 & 6

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor()
+  {
+    super()
+    this.state = {
+      friends: [],
+      picture: '',
+      name: ''
+    }
+  }
+  updatePicture(value) 
+  {
+    this.setState({picture:value})
+  }
+  updateName(value)
+  {
+    this.setState({name:value})
+  }
   render() {
-    const names = ["Aye", "Bee", "See", 'Dee', "Eee"]
     return (
       <div>
-        Picture: <input/>
-        Name: <input/>
+        Picture: <input onChange={event => this.updatePicture(event.target.value)} value={this.state.picture}/>
+        Name: <input onChange={event=> this.updateName(event.target.value)} value={this.state.picture}/>
         <button>Save it</button>
+        <div>State: {JSON.stringify(this.state)}</div>
       </div>
     );
 
